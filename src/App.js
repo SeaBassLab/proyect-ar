e84cf2219fd0910a1cf4643b542b911b4db8f6c8
root <root@Lakshmi.localdomain>
2021-03-27 23:13:48 -0300

    change pakage.json

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import React, { Suspense } from 'react';
 import { CssBaseline } from '@material-ui/core';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import Home from './pages/Home';
-import NotFound from './pages/NotFound';
 import Layout from './components/Layout';
 
 const App = () => {
@@ -12,8 +11,7 @@ const App = () => {
         <BrowserRouter>
           <Layout>
             <Switch>
-              <Route path="/" exact component={Home} />
-              <Route component={NotFound} />
+              <Route exact path="/JPSP/" component={Home} />
             </Switch>
           </Layout>
         </BrowserRouter>
